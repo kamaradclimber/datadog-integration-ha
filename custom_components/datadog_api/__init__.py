077a55513dc7254023dc734d2c6989083467af9b
Grégoire Seux <grego_github@familleseux.net>
2024-06-07 21:03:27 +0200

    ✨ Working metrics send
    It very very bare but seems to work for "easy" cases (metrics whose
    state is trivial to convert)

--- custom_components/datadog_api/__init__.py
+++ custom_components/datadog_api/__init__.py
@@ -1,17 +1,27 @@
 import logging
+from functools import partial
 
-from homeassistant.const import Platform, EVENT_HOMEASSISTANT_STARTED
-from homeassistant.core import HomeAssistant, callback, HassJob, Event
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.metrics_api import MetricsApi
+from datadog_api_client.v2.model.metric_content_encoding import MetricContentEncoding
+from datadog_api_client.v2.model.metric_intake_type import MetricIntakeType
+from datadog_api_client.v2.model.metric_payload import MetricPayload
+from datadog_api_client.v2.model.metric_point import MetricPoint
+from datadog_api_client.v2.model.metric_series import MetricSeries
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.events_api import EventsApi
+from datadog_api_client.v1.model.event_create_request import EventCreateRequest
+
+from homeassistant.const import Platform, EVENT_STATE_CHANGED
+from homeassistant.core import HomeAssistant, Event, EventStateChangedData, State
 from homeassistant.config_entries import ConfigEntry
 from .const import DOMAIN
-from datetime import datetime, timedelta
-from homeassistant.loader import async_get_integrations
-from homeassistant.setup import async_get_loaded_integrations
-from homeassistant.helpers.event import async_call_later, async_track_time_interval
+from datetime import datetime
 from homeassistant.helpers.storage import Store
-from awesomeversion import AwesomeVersion
 from homeassistant.const import __version__
-from homeassistant.helpers.entity_registry import async_get
+from homeassistant.helpers.state import state_as_number
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,6 +37,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     # subscribe to config updates
     entry.async_on_unload(entry.add_update_listener(update_entry))
+    
+    event_listener=partial(full_event_listener, entry.data)
+
+
+    unsubscribe = hass.bus.async_listen(EVENT_STATE_CHANGED, event_listener)
+    hass.data[DOMAIN][entry.entry_id]["unsubscribe_handler"] = unsubscribe
+
 
     return True
 
@@ -47,4 +64,57 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(
         entry, [Platform.SENSOR]
     )
+    if "unsubscribe_handler" in hass.data[DOMAIN][entry.entry_id]:
+        hass.data[DOMAIN][entry.entry_id]["unsubscribe_handler"]()
     return unload_ok
+
+PREFIX = "hass"
+
+def full_event_listener(creds: dict, event: Event[EventStateChangedData]):
+    _LOGGER.info("Receive new state change")
+    _LOGGER.info(event)
+
+    new_state = event.data["new_state"]
+    if new_state is None:
+        _LOGGER.warn("This event has no new state, isn't it strange?")
+        return
+    domain = new_state.domain if new_state.domain else new_state.entity_id.split(".")[0]
+    metric_name = f"{PREFIX}.{domain}".replace(" ", "_")
+    state = new_state.state
+    try:
+        state_as_number(new_state)
+    except:
+        # we cannot treat this kind of event
+        _LOGGER.warn(f"Cannot treat this state changed event: {event} to convert to metric")
+        return
+    value = (
+            state
+            if isinstance(state, (float, int))
+            else state_as_number(new_state)
+            )
+    tags = [f"entity:{new_state.entity_id}"]
+    unit = None
+    if "friendly_name" in new_state.attributes:
+        tags.append(f"friendly_name:{new_state.attributes['friendly_name']}")
+        unit = (new_state.attributes["unit_of_measurement"] if "unit_of_measurement" in new_state.attributes else None)
+    timestamp = new_state.last_changed
+    if value is None:
+        # Ignore event if state is not number and cannot be converted to a number
+        return
+    if value is not None:
+        payload = MetricPayload(series=[
+            MetricSeries(metric=metric_name, type=MetricIntakeType.GAUGE, tags=tags, unit=unit,
+                           points=[MetricPoint(timestamp=int(timestamp.timestamp()), value=value)])
+        ])
+        configuration = Configuration(
+                api_key={"apiKeyAuth": creds["api_key"],
+                         },
+                server_variables={"site": creds["site"]},
+                request_timeout=5,
+                enable_retry=True,
+        )
+        with ApiClient(configuration) as api_client:
+            api_instance = MetricsApi(api_client)
+            response = api_instance.submit_metrics(content_encoding=MetricContentEncoding.ZSTD1, body=payload)
+            if len(response["errors"]) > 0:
+                _LOGGER.error(f"Error sending metric to Datadog {response['errors'][0]}")
--- custom_components/datadog_api/config_flow.py
+++ custom_components/datadog_api/config_flow.py
@@ -1,6 +1,11 @@
 import logging
 from typing import Any, Optional, Tuple
 import voluptuous as vol
+from datadog_api_client import AsyncApiClient, Configuration
+from datadog_api_client.v2.api.metrics_api import MetricsApi
+
+
+from homeassistant.exceptions import ConfigEntryAuthFailed, HomeAssistantError
 from homeassistant.core import callback, HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
@@ -29,4 +34,29 @@ class SetupConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self.data = {}
 
     async def async_step_user(self, user_input: Optional[dict[str, Any]] = None):
-        return self.async_create_entry(title="Datadog via API", data=self.data)
+        if user_input is not None:
+
+            configuration = Configuration(
+                    api_key={"apiKeyAuth": user_input["api_key"],
+                             },
+                    server_variables={"site": user_input["site"]},
+                    request_timeout=5,
+                    enable_retry=True,
+            )
+            try:
+                #async with AsyncApiClient(configuration) as api_client:
+                #    client = MetricsApi(api_client)
+                #    await client.list_tag_configurations()
+                pass
+            except Exception as err:
+                raise ConfigEntryAuthFailed(err) from err
+            
+            await self.async_set_unique_id(user_input["api_key"])
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(title="Datadog via API", data=user_input)
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema({
+                vol.Required("api_key"): str,
+                vol.Required("site"): str,
+            })
+        )
