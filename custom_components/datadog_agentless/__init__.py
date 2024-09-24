5ee0903ff700246e4438746f299dce63ddaca14f
Grégoire Seux <grego_github@familleseux.net>
2024-09-24 19:48:23 +0200

    🔇 Remove even more attribute related entities
    Those cannot be converted to numerical values

--- custom_components/datadog_agentless/__init__.py
+++ custom_components/datadog_agentless/__init__.py
@@ -164,7 +164,11 @@ def ignore_by_entity_id(entity_id: str) -> bool:
     # powercalc attributes
     if re.match(".+_device_power_attribute_.+", entity_id):
         return True
-    if re.match(".+_attribute_(source|sourcedomain|sourceentity|energysensorentityid|integration|calculationmode|priceentity|energyentity)", entity_id):
+    if re.match(".+_attribute_(source|sourcedomain|sourceentity|energysensorentityid|integration|calculationmode|priceentity|energyentity|status|accesstoken|attribution|devicename|deviceid|batterytypeandquantity|batterytype)", entity_id):
+        return True
+    if re.match(".+unit", entity_id):
+        return True
+    if re.match("", entity_id):
         return True
 
     return False
